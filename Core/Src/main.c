6d52521d9494c15f7cfc075bfd3a649b11ce99ee
Aldrin Rebellow <aldrin.rebellow@nestgroup.net>
2026-04-27 21:35:50 +0530

    uopate

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -397,9 +397,6 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, SW2_INP_Pin|SW3_INP_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, MOT1_PUL__Pin|MOT2_PUL__Pin|MOT2_DISABLE__Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
@@ -411,17 +408,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SW1_INP_Pin SW4_INP_Pin */
-  GPIO_InitStruct.Pin = SW1_INP_Pin|SW4_INP_Pin;
+  /*Configure GPIO pins : SW1_INP_Pin SW2_INP_Pin SW3_INP_Pin SW4_INP_Pin */
+  GPIO_InitStruct.Pin = SW1_INP_Pin|SW2_INP_Pin|SW3_INP_Pin|SW4_INP_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : SW2_INP_Pin SW3_INP_Pin MOT2_DISABLE__Pin */
-  GPIO_InitStruct.Pin = SW2_INP_Pin|SW3_INP_Pin|MOT2_DISABLE__Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : SW5_INP_Pin SW6_INP_Pin */
@@ -442,6 +432,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(MOT1_DISABLE__GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : MOT2_DISABLE__Pin */
+  GPIO_InitStruct.Pin = MOT2_DISABLE__Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(MOT2_DISABLE__GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : MOT2_DIR__Pin MOT1_DIR__Pin */
   GPIO_InitStruct.Pin = MOT2_DIR__Pin|MOT1_DIR__Pin;
