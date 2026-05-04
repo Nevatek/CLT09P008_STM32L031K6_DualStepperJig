f3bd7c19068d9af31dafb9982ee7b8f9a4683932
Afil <afilkvkv@gmail.com>
2026-05-05 01:45:17 +0530

    Changed structure data , Added reset buttons (TODO sectio for fuction add), Added updated html

--- Core/Application/Appl_PacketHandler.c
+++ Core/Application/Appl_PacketHandler.c
@@ -19,7 +19,7 @@ Author Date Description
 #include "Appl_PacketHandler.h"
 
 /*********************** globals *********************************************/
-static SystemCofig_t g_Config;
+static SystemCofig_t g_Config = {0};
 
 PacketHandlerState_t PacketHandler_state;
 
@@ -33,88 +33,86 @@ extern void Callback_Appl_ConfigUpdated(SystemCofig_t *pConfig);
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 static uint16_t Packet_crc16(uint8_t *data, uint16_t len)
 {
-    uint16_t crc = 0xFFFF;
+	uint16_t crc = 0xFFFF;
 
-    for (uint16_t i = 0; i < len; i++)
-    {
-        crc ^= (data[i] << 8);
+	for (uint16_t i = 0; i < len; i++)
+	{
+		crc ^= (data[i] << 8);
 
-        for (uint8_t j = 0; j < 8; j++)
-        {
-            if (crc & 0x8000)
-                crc = (crc << 1) ^ 0x1021;
-            else
-                crc <<= 1;
-        }
-    }
-    return crc;
+		for (uint8_t j = 0; j < 8; j++)
+		{
+			if (crc & 0x8000)
+				crc = (crc << 1) ^ 0x1021;
+			else
+				crc <<= 1;
+		}
+	}
+	return crc;
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 void PacketHandler_Send(uint8_t cmd, uint8_t *data, uint8_t len)
 {
-    uint8_t buf[300];
-    uint16_t idx = 0;
+	uint8_t buf[300];
+	uint16_t idx = 0;
 
-    buf[idx++] = PACKET_SOP;
-    buf[idx++] = cmd;
-    buf[idx++] = len;
+	buf[idx++] = PACKET_SOP;
+	buf[idx++] = cmd;
+	buf[idx++] = len;
 
-    for (uint8_t i = 0; i < len; i++)
-        buf[idx++] = data[i];
+	for (uint8_t i = 0; i < len; i++)
+		buf[idx++] = data[i];
 
-    uint16_t crc = Packet_crc16(&buf[1], 2 + len); // CMD+LEN+DATA
+	uint16_t crc = Packet_crc16(&buf[1], 2 + len); // CMD+LEN+DATA
 
-    buf[idx++] = (crc >> 8) & 0xFF;
-    buf[idx++] = crc & 0xFF;
+	buf[idx++] = (crc >> 8) & 0xFF;
+	buf[idx++] = crc & 0xFF;
 
-    uart_Transmit(buf, idx);
+	uart_Transmit(buf, idx);
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
-bool PacketHandler_Decode(uint8_t *data, uint8_t len)
+ ******************************************************************************/
+bool PacketHandler_Decode(const uint8_t *data)
 {
-    if (PAYLOAD_LEN != g_PacketBuffer[2])
-        return false;
+	if (PAYLOAD_LEN != g_PacketBuffer[2])
+		return false;
 
-    // M1
-    g_Config.motor1.steps =data[0] | (data[1] << 8);
-    g_Config.motor1.dir   =data[2];
-    g_Config.motor1.home  =data[3];
-    g_Config.motor1.mode  =data[4];
-    g_Config.motor1.ustep =data[5];
-    g_Config.motor1.freq  =data[6] | (data[7] << 8);
+	for (int i = 0; i < 2; i++)
+	{
+		const uint8_t  *b  = data + (i * 12);
+		MotorConfig_t  *m  = (i == 0) ? &g_Config.motor1 : &g_Config.motor2;
 
-    // M2
-    g_Config.motor2.steps =data[8]  | (data[9] << 8);
-    g_Config.motor2.dir   =data[10];
-    g_Config.motor2.home  =data[11];
-    g_Config.motor2.mode  =data[12];
-    g_Config.motor2.ustep =data[13];
-    g_Config.motor2.freq  =data[14] | (data[15] << 8);
+		m->steps          = (uint16_t)(b[0] | (b[1] << 8));
+		m->dir            = b[2];
+		m->home           = b[3];
+		m->mode           = b[4];
+		m->ustep          = b[5];          /* log2 → divisor */
+		m->freq           = (uint16_t)(b[6] | (b[7] << 8));
+		m->u8StepAngle    = (uint16_t)(b[8] | (b[9] << 8));
+		m->u8StrokeDelayMs= (uint16_t)(b[10] | (b[11] << 8));
+	}
 
-    // Sync
-    g_Config.sync =data[16];
+	g_Config.sync = data[24];
 
-    return true;
+	return true;
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 static void send_ack(void)
 {
 	PacketHandler_Send(PACKET_CMD_ACK, 0, 0);
@@ -124,7 +122,7 @@ static void send_ack(void)
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 static void send_error(uint8_t err)
 {
 	PacketHandler_Send(PACKET_CMD_ERR, &err, 1);
@@ -134,29 +132,29 @@ static void send_error(uint8_t err)
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 static void send_cycles(uint32_t m1, uint32_t m2)
 {
-    uint8_t d[8];
+	uint8_t d[8];
 
-    d[0] = (m1 >> 0);
-    d[1] = (m1 >> 8);
-    d[2] = (m1 >> 16);
-    d[3] = (m1 >> 24);
+	d[0] = (m1 >> 0);
+	d[1] = (m1 >> 8);
+	d[2] = (m1 >> 16);
+	d[3] = (m1 >> 24);
 
-    d[4] = (m2 >> 0);
-    d[5] = (m2 >> 8);
-    d[6] = (m2 >> 16);
-    d[7] = (m2 >> 24);
+	d[4] = (m2 >> 0);
+	d[5] = (m2 >> 8);
+	d[6] = (m2 >> 16);
+	d[7] = (m2 >> 24);
 
-    PacketHandler_Send(PACKET_CMD_CYC, d, 8);
+	PacketHandler_Send(PACKET_CMD_CYC, d, 8);
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 void Appl_PacketHandler_UpdateCycles(uint32_t m1, uint32_t m2)
 {
 	motor1_Count = m1;
@@ -166,60 +164,86 @@ void Appl_PacketHandler_UpdateCycles(uint32_t m1, uint32_t m2)
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 static uint8_t validate_cfg(SystemCofig_t *cfg)
 {
-    if (cfg->motor1.steps == 0 || cfg->motor2.steps == 0)
-        return PACKET_ERR_INVALID_STEPS;
+	if (cfg->motor1.steps == 0 || cfg->motor2.steps == 0)
+		return PACKET_ERR_INVALID_STEPS;
 
-    if (cfg->motor1.freq == 0 || cfg->motor2.freq == 0)
-        return PACKET_ERR_INVALID_FREQ;
+	if (cfg->motor1.freq == 0 || cfg->motor2.freq == 0)
+		return PACKET_ERR_INVALID_FREQ;
 
-    return 0;
+	return 0;
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
-void PacketHandler_Process(uint8_t *data)
+ ******************************************************************************/
+void PacketHandler_Process()
 {
-	    switch (data[1])
-	    {
-	        case PACKET_CMD_CFG:
-	        {
-	            if (!PacketHandler_Decode(&(data[3]), g_BufferSize))
-	            {
-	                send_error(PACKET_ERR_GENERIC);
-	                return;
-	            }
+	uint8_t pkt_cmd = g_PacketBuffer[1];
+	switch (pkt_cmd)
+	{
+	case PACKET_CMD_CFG:
+	{
+		if (!PacketHandler_Decode(&(g_PacketBuffer[3])))
+		{
+			send_error(PACKET_ERR_GENERIC);
+			return;
+		}
 
-	            uint8_t err = validate_cfg(&g_Config);
-	            if (err)
-	            {
-	                send_error(err);
-	                return;
-	            }
+		uint8_t err = validate_cfg(&g_Config);
+		if (err)
+		{
+			send_error(err);
+			return;
+		}
 
-	            send_ack();
+		send_ack();
 
-				g_CountSentTime = HAL_GetTick();
+		g_CountSentTime = HAL_GetTick();
 
-	            break;
-	        }
+		break;
+	}
 
-	        default:
-	            send_error(PACKET_ERR_GENERIC);
-	            break;
-	    }
+	case PACKET_CMD_RST:
+	{
+		uint8_t mask = g_PacketBuffer[3];
+		if (mask & 0x01)
+		{
+			/* reset Motor 1 */
+			//TODO: Call function to reset count
+			send_ack();
+
+		}
+		else if (mask & 0x02)
+		{
+			/* reset Motor 2 */
+			//TODO: Call function to reset count
+			send_ack();
+
+		}
+		else
+		{
+			send_error(PACKET_ERR_GENERIC);
+
+		}
+		break;
+	}
+
+	default:
+		send_error(PACKET_ERR_GENERIC);
+		break;
+	}
 }
 
 /******************************.FUNCTION_HEADER.******************************
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 void Appl_PacketHandler_Init(void)
 {
 	uart_Init();
@@ -233,7 +257,7 @@ void Appl_PacketHandler_Init(void)
 .Purpose : This function serve as one time call function of application layer
 .Returns :
 .Note : use this function for all major initilization
-******************************************************************************/
+ ******************************************************************************/
 void Appl_PacketHandler_Exe(void)
 {
 	if((HAL_GetTick() - g_CountSentTime) >= TICK_1_SEC)
@@ -272,7 +296,7 @@ void Appl_PacketHandler_Exe(void)
 	case HANDLER_PROCESS_DATA:
 	{
 
-		PacketHandler_Process(g_PacketBuffer);
+		PacketHandler_Process();
 		Callback_Appl_ConfigUpdated(&g_Config);
 
 		PacketHandler_state = HANDLER_IDLE;
